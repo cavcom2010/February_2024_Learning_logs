30777240c66bdab7b15a9becaf44a93e44cb1b11
cavcom2010 <email@email.com>
2024-02-10 09:46:04 +0000

    modified the button in css

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("new_topic/", views.new_topic, name="new_topic"),
     path("topic/<int:topic_id>/new_entry/", views.new_entry, name="new_entry"),
     path("edit_entry/<int:entry_id>/", views.edit_entry, name="edit_entry"),
+    # path("topic/<int:topic_id>/edit_entry/<int:entry_id>/", views.edit_entry, name="edit_entry"),
+         
+
 ]
