733cb7a257d95d4b11a1c4b2da182ca36224c1f3
cavcom2010 <admin@admin.com>
2024-02-16 14:21:40 +0000

    changed settings and urls to remove users

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 
     'learning_logs',
-    'users',
+
 
 
     'django.contrib.admin',
--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("users/", include("users.urls")),
     path("", include("learning_logs.urls")),
 ]
