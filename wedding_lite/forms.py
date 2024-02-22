59dfcbfb63868eecbfc9074f1af303534caf3111
cavcom2010 <email@email.com>
2024-02-22 07:32:20 +0000

    added the backend rsvp form

--- wedding_lite/forms.py
+++ wedding_lite/forms.py
@@ -5,13 +5,8 @@ class RSVPForm(forms.ModelForm):
     class Meta:
         model = RSVP
         fields = ['full_name', 'email_address', 'number_of_guests', 'message']
-        widgets = {
-            'full_name': forms.TextInput(attrs={'class': 'form-control-sm'}),
-            'email_address': forms.EmailInput(attrs={'class': 'form-control-sm'}),
-            'number_of_guests': forms.Select(attrs={'class': 'form-control-sm'}),
-            'date_of_rsvp': forms.DateInput(attrs={'class': 'form-control-sm'}),
-          'message': forms.Textarea(attrs={'class': 'form-control-sm'}),
-        }
+        
+        
 
 
 
--- wedding_lite/models.py
+++ wedding_lite/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class RSVP(models.Model):
-    full_name = models.CharField(max_length=100, unique=True)
+    full_name = models.CharField(max_length=100, unique=True, help_text="Enter your Full Name")
     email_address = models.EmailField(max_length=100)
     number_of_guests = models.IntegerField(choices=[(i, i) for i in range(1, 7)])
     date_of_rsvp = models.DateField(auto_now_add=True)
