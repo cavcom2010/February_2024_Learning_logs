6cdaa5d192494825cf81d15e6d13eb86f54848b6
cavcom2010 <email@email.com>
2024-02-22 08:25:53 +0000

    Revert "Revert "styling the new rsvp form""
    This reverts commit 938c83ba2de6f832f6a37b94235912c016088f6d.

--- wedding_lite/views.py
+++ wedding_lite/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .import views
+from .models import RSVP
+from .forms import RSVPForm
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'index.html')
+    rsvp = RSVP.objects.all()
+    return render(request, 'index.html', {'rsvp': rsvp})
+
+def rsvp_view(request):
+    if request.method == 'POST':
+        form = RSVPForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = RSVPForm()
+
+    context = {'form': form}
+    return render(request, 'rsvp.html', context)
+
+    
+
 
