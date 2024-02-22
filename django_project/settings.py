deb753861db228d4729494e87f4fb8b8e926b90f
cavcom2010 <email@email.com>
2024-02-22 16:30:21 +0000

    done collectstatic

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+    # BASE_DIR / 'static',
+# ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -135,3 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # MEDIA_ROOT = "media"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
